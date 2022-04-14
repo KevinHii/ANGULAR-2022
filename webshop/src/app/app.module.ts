d8049305d9d3d38d59a860dc97b6779161cfd416
KevinHii <102091702+KevinHii@users.noreply.github.com>
2022-04-14 12:13:01 +0300

    uue projekti põhi

--- webshop/src/app/app.module.ts
+++ webshop/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { AddProductComponent } from './admin/add-product/add-product.component';
+import { EditProductComponent } from './admin/edit-product/edit-product.component';
+import { ViewProductComponent } from './admin/view-product/view-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    AdminHomeComponent,
+    AddProductComponent,
+    EditProductComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
